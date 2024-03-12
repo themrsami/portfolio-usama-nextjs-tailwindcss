e4e337559809be783062c36eb7544e69adaf8621
themrsami <shamastabrez1313@gmail.com>
2024-03-12 13:55:58 +0500

    active links method changed

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,8 +1,7 @@
 'use client';
 import Link from 'next/link'
 import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { usePathname } from 'next/navigation';
 import { BiHomeHeart } from "react-icons/bi";
 import { BiSolidUser } from "react-icons/bi";
 import { BiSolidCategoryAlt } from "react-icons/bi";
@@ -13,15 +12,6 @@ import { ThemeContext } from './ThemeContext';
 import { useContext } from 'react';
 
 const Navbar = () => {
-    const [activeLink, setActiveLink] = useState('');
-
-    useEffect(() => {
-      setActiveLink(window.location.pathname);
-    }, []);
-  
-    const handleClick = (path: string) => {
-      setActiveLink(path);
-    };
 
     const { isToggled } = useContext(ThemeContext); // Use the useContext hook to access isToggled
 
@@ -29,14 +19,14 @@ const Navbar = () => {
     const activenav = isToggled ? 'active-nav-dark' : 'active-nav';
     const inactivenav = isToggled ? 'inactive-nav-dark' : 'inactive-nav';
     const nameheading = isToggled ? 'name-heading' : 'name-heading-light';
+    const pathname = usePathname();
 
 
   return (
     <>
         <nav className={`fixed z-[9999] bottom-8 left-0 right-0 flex justify-center mx-auto items-center max-w-[280px] py-2 mt-6 rounded-full backdrop-blur-sm bg-white/10 ... ring-2 ${navring}`}>
             <ul className='flex w-[85%] justify-between mx-auto'>
-                <Link href='/' className={activeLink === '/' ? activenav : inactivenav} 
-            onClick={() => handleClick('/')}>
+                <Link href='/' className={pathname === '/' ? activenav : inactivenav} >
                     <li className='group'>
                         <BiHomeHeart/>
                         <span className="absolute text-black -top-8 left-[15%] -translate-x-[10%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
@@ -44,8 +34,7 @@ const Navbar = () => {
                         </span>
                     </li>
                 </Link>
-                <Link href='/About'  className={activeLink === '/About' ? activenav : inactivenav} 
-            onClick={() => handleClick('/About')}>
+                <Link href='/About'  className={pathname === '/About' ? activenav : inactivenav}>
                     <li className='group'>
                         <BiSolidUser/>
                         <span className="absolute text-black -top-8 left-[35%] -translate-x-[20%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
@@ -53,8 +42,7 @@ const Navbar = () => {
                         </span>
                     </li>
                 </Link>
-                <Link href='/Services'  className={activeLink === '/Services' ? activenav : inactivenav} 
-            onClick={() => handleClick('/Services')}>
+                <Link href='/Services'  className={pathname === '/Services' ? activenav : inactivenav}>
                     <li className='group'>
                         <BiSolidCategoryAlt/>
                         <span className="absolute text-black -top-8 left-[50%] -translate-x-[10%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
@@ -62,8 +50,7 @@ const Navbar = () => {
                         </span>
                     </li>
                 </Link>
-                <Link href='/Portfolio' className={activeLink === '/Portfolio' ? activenav : inactivenav} 
-            onClick={() => handleClick('/Portfolio')}>
+                <Link href='/Portfolio' className={pathname === '/Portfolio' ? activenav : inactivenav} >
                     <li className='group'>
                         <BiBlanket/>
                         <span className="absolute text-black -top-8 left-[70%] -translate-x-[10%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
@@ -71,8 +58,7 @@ const Navbar = () => {
                         </span>
                     </li>
                 </Link>
-                <Link href='/Contact'  className={activeLink === '/Contact' ? activenav : inactivenav}
-            onClick={() => handleClick('/Contact')}>
+                <Link href='/Contact'  className={pathname === '/Contact' ? activenav : inactivenav}>
                     <li className='group'>
                         <BiPhone/>
                         <span className="absolute text-black -top-8 left-[90%] -translate-x-[10%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
