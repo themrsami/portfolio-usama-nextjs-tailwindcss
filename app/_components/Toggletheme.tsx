64592fcad75abb53c04f96c49aa743a4167bb079
Usama Nazir <usamanazir13@gmail.com>
2024-06-08 21:28:20 +0500

    added toast for theme changing

--- app/_components/Toggletheme.tsx
+++ app/_components/Toggletheme.tsx
@@ -1,17 +1,30 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from './ThemeContext';
 import { BiMoon, BiSun } from 'react-icons/bi';
+import { useToast } from "@/components/ui/use-toast";
 
 const Toggletheme = () => {
-    const { isToggled, handleToggle } = useContext(ThemeContext);
+  const { isToggled, handleToggle } = useContext(ThemeContext);
+  const { toast } = useToast();
 
-    return (
-        <div>
-            <button className='flex justify-center items-center' onClick={handleToggle}>
-                {isToggled ? <BiSun className='text-4xl text-black bg-yellow-500 p-2 rounded-full'/> : <BiMoon className='text-4xl text-white bg-black p-2 rounded-full'/>}
-            </button>
-        </div>
-    );
+  const toggleTheme = () => {
+    handleToggle();
+    toast({
+      title: `Switched to ${isToggled ? "light" : "dark"} mode`,
+    });
+  };
+
+  return (
+    <div>
+      <button className='flex justify-center items-center' onClick={toggleTheme}>
+        {isToggled ? (
+          <BiSun className='text-4xl text-black bg-yellow-500 p-2 rounded-full' />
+        ) : (
+          <BiMoon className='text-4xl text-white bg-black p-2 rounded-full' />
+        )}
+      </button>
+    </div>
+  );
 };
 
 export default Toggletheme;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/Navbar";
 import { ThemeProvider } from '@/app/_components/ThemeProvider';
+import { Toaster } from "@/components/ui/toaster";  // Import Toaster component
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,12 +22,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider>
-            <Navbar/>
-            <div className="relative flex top-24 left-0 right-0 bottom-28 justify-center mx-auto container w-[100%] h-[100%] rounded-[10px]">
-              {children}
-            </div>
-          </ThemeProvider>
-        </body>
+          <Navbar/>
+          <div className="relative flex top-24 left-0 right-0 bottom-28 justify-center mx-auto container w-[100%] h-[100%] rounded-[10px]">
+            {children}
+          </div>
+          <Toaster />  {/* Add the Toaster component here */}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
